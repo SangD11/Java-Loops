6dbc2256e275c14e99ed8e01329855c096b422ad
SangD11 <116387184+SangD11@users.noreply.github.com>
2023-03-08 18:41:57 -0500

    Add files via upload

---
+++ ifElse.java
@@ -0,0 +1,31 @@
+package Loops;
+
+public class ifElse {
+    public static void main(String[] args) {
+       // int a = 9;
+        //if (a > 10) {
+          //  System.out.println("hello");
+        //} else {
+          //  System.out.println("wrong");
+        //}
+        // if A is greater 5 print a less than 5
+        // if A equal to 7 print equal to 7
+        //otherwise print nothing
+        int a =10;
+        if(a<5) {
+            System.out.println("a less than 5 ");
+        } else if (a == 5) {
+            System.out.println("7");
+        }else{
+            System.out.println("Nothing");
+        }
+        //Write the program to hold boolean value and if the boolean value is true print i won the lottery otherwise
+        //i lost
+
+       // boolean var = true;
+       // if (var == true){
+            //System.out.println("i won the Lottery");
+       // }else{
+           // System.out.println("i lost");
+       // }
+}}
