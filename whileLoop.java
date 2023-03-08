09b753b5b5bd0e717ad1a65447a1a4b8a652eca3
SangD11 <116387184+SangD11@users.noreply.github.com>
2023-03-08 18:42:46 -0500

    Add files via upload

---
+++ whileLoop.java
@@ -0,0 +1,16 @@
+package Loops;
+
+public class whileLoop {
+    public static void main(String[] args) {
+        // int a = 1;
+        //while (a<=10){
+        //System.out.println(a);
+        //a++;
+        // }
+        int a = 10;
+        while (a > 1) {
+            System.out.println(a);
+            a--;
+        }
+    }
+}
