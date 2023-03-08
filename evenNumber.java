952911ad92569694136a4ed824d65c991af0585f
SangD11 <116387184+SangD11@users.noreply.github.com>
2023-03-08 18:41:35 -0500

    Add files via upload

---
+++ evenNumber.java
@@ -0,0 +1,27 @@
+package Loops;
+
+public class evenNumber {
+    public static void main(String[] args) {
+       int a;
+       for(a = 1; a <= 10; ++a) {
+           if (a % 2 == 0) {
+               System.out.println(a);
+            }
+        }
+
+       // for(a = 1; a <= 10; a++) {
+         //  if (a % 2 == 1) {
+           //   System.out.println(a);
+           //}
+        //}
+       // for(a = 10; a <= 10; a--) {
+            //if (a % 2 == 1) {
+               // System.out.println(a);
+           // }
+        //}
+       // for(a = 1; a <= 100; a+=3) {
+         //   System.out.println(a);
+           // }
+        }
+    }
+
