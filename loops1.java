9df47b1747bea9d16aa9d817b0f875fbf6f94343
SangD11 <116387184+SangD11@users.noreply.github.com>
2023-03-08 18:42:22 -0500

    Add files via upload

---
+++ loops1.java
@@ -0,0 +1,30 @@
+package Loops;
+
+public class loops1 {
+    public static void main(String[] args) {
+       // for(int a = 1; a <10; a++) {
+       // System.out.println(a);
+    //}
+        //for(int a = 1; a <= 5; a++) {
+            //System.out.println(a);
+       // }
+        //for(int a = 10; a >= 1; a++) {
+           // System.out.println(a);
+       // }
+       // for(int a = 10; a >= 5; a--) {
+           // System.out.println(a);
+      //  }
+       for(int a =0; a<= 20; a++) {
+        if (a >= 5) {
+            break;
+        }
+        System.out.println(a);
+    }
+        for(int a =0; a<= 20; a++) {
+            if (a == 5) {
+                break;
+            }
+            System.out.println(a);
+        }
+
+}}
